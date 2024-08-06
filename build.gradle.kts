9319929a5ae2bf4229ec9696a25b95fb8ebd8d28
Siddhartha Ghimire <engr.siddhartha@gmail.com>
2024-08-06 08:40:59 +0545

    Defined flavor, test logging and optimized build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,21 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("minSdk", 23)
+        set("targetSdk", 34)
+        set("compileSdk", 34)
+        set("applicationId", "np.com.siddharthaghimire.shuffly")
+        set("versionName", "letup")
+        set("androidJUnitRunner", "androidx.test.runner.AndroidJUnitRunner")
+        set("buildTypeDebug", "debug")
+        set("buildTypeRelease", "release")
+        set("jvmTargetVersion", "17")
+        set("proguardOptimize", "proguard-android-optimize.txt")
+        set("proguardRulesPro", "proguard-rules.pro")
+        set("appName", "Shuffly")
+    }
+}
+
 plugins {
     alias(libs.plugins.androidApplication) apply false
     alias(libs.plugins.jetbrainsKotlinAndroid) apply false
